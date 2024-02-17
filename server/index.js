dd6505488bc9bda981959df8f27acd2d33b39bca
AlessandrodeAlmeida2 <agostini55@gmail.com>
2024-02-17 09:59:10 -0300

    alteração no style ListaProdutos

--- server/index.js
+++ server/index.js
@@ -9,7 +9,8 @@ app.use(cors()); // Adicione esta linha
 app.get('/produtos', async (req, res) => {
     try {
         const response = await axios.get('https://makeup-api.herokuapp.com/api/v1/products.json');
-        res.json(response.data);
+        const data = response.data.filter(product => product.image_link && product.image_link.trim() !== '' && product.price && product.price > 0.0);
+        res.json(data);
     } catch (error) {
         console.error(error);
         res.status(500).send('Erro ao buscar produtos');
--- src/components/ListaProdutos.vue
+++ src/components/ListaProdutos.vue
@@ -1,12 +1,29 @@
 <template>
-    <div>
-      <div v-for="produto in produtos" :key="produto.id">
+    <div class="produto">
+      <div class="pr" v-for="produto in produtos" :key="produto.id">
         <h2>{{ produto.name }}</h2>
         <img :src="produto.image_link" alt="">
-        <p>{{ produto.price }}</p>
+        <p>R$ {{ produto.price }}</p>
       </div>
     </div>
   </template>
+
+<style scoped>
+.produto {
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-evenly;
+  align-items: flex-start;
+}
+.pr {
+  padding: 10px;
+}
+.produto img {
+  height: 100%;
+  max-height: 200px;
+  width: auto;
+}
+</style>
   
   <script>
   export default {
